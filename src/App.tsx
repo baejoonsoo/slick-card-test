5d0fe064d6dfb2236fa93cf29d4d50ce305c5cc9
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-04-06 22:14:23 +0900

    카드 뒤로가기 애니메이션
    정지원 선배 채고...

--- src/App.tsx
+++ src/App.tsx
@@ -1,123 +1,51 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 
-const CARD_LIST = [1, 2, 3, 4, 5];
+let list = [1, 2, 3, 4, 5];
 
-function App() {
-  const [count, setCount] = useState<number>(0);
-  const [cardList, setCardList] = useState<number[]>([]);
-
-  const preCardRef = useRef<HTMLDivElement>(null);
-  const centerCardRef = useRef<HTMLDivElement>(null);
-  const nextCardRef = useRef<HTMLDivElement>(null);
-
-  const getCardList = () => {
-    const cards = [count];
-
-    cards.push((count + 1) % CARD_LIST.length);
-
-    const unSiiftNum1 = (count - 1) % CARD_LIST.length;
-    cards.unshift(
-      unSiiftNum1 >= 0 ? unSiiftNum1 : CARD_LIST.length + unSiiftNum1
-    );
-
-    console.log(cards);
-    setCardList(cards);
-  };
-
-  useEffect(() => {
-    getCardList();
-    setCount(1);
-  }, []);
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     if (centerCardRef && centerCardRef.current) {
-  //       centerCardRef.current.style.transform = ``;
-  //     }
-  //   }, 500);
-  // }, [cardList]);
-
-  const showNextCardList = () => {
-    if (centerCardRef && centerCardRef.current) {
-      // centerCardRef.current.style.transform = `translateX(-112px)`;
-    }
-  };
-
-  const showPreCardList = () => {
-    if (centerCardRef && centerCardRef.current) {
-      // centerCardRef.current.style.transform = `translateX(112px)`;
-    }
-  };
-
-  const addCount = () => {
-    setCount((prev) => {
-      return prev === CARD_LIST.length - 1 ? 0 : prev + 1;
-    });
-    showNextCardList();
-    getCardList();
-  };
-
-  const delCount = () => {
-    setCount((prev) => (prev === 0 ? CARD_LIST.length - 1 : prev - 1));
-    showPreCardList();
-    getCardList();
+export default function App() {
+  const [tempList, setTempList] = useState([4, 5, 1, 2, 3]);
+  // [3,4,5,1,2]
+  const before = () => {
+    const temp = tempList;
+    const end = tempList[tempList.length - 1];
+    temp.length = tempList.length - 1;
+    temp.splice(0, 0, end);
+    setTempList([...temp]);
   };
 
   return (
-    <div>
-      <button onClick={addCount}>add</button>
-      <button onClick={delCount}>d</button>
-      <Container>
-        <Card ref={preCardRef} id="pre">
-          {cardList[0]}
+    <Wrap>
+      <button onClick={before}>뒤로</button>
+      <button>앞으로</button>
+      {list.map((_, index) => (
+        <Card
+          key={index}
+          style={{ marginLeft: `${tempList.indexOf(_) * 100}px` }}
+        >
+          {_}
         </Card>
-        <Card ref={centerCardRef} id="center">
-          {cardList[1]}
-        </Card>
-        <Card ref={nextCardRef} id="next">
-          {cardList[2]}
-        </Card>
-      </Container>
-    </div>
+      ))}
+    </Wrap>
   );
 }
 
-export default App;
-
-const Container = styled.div`
+const Wrap = styled.div`
   display: flex;
-  width: 100%;
-  height: 100vh;
-  align-items: center;
-  justify-content: center;
+  & button {
+    background-color: grey;
+    cursor: pointer;
+  }
 `;
 
 const Card = styled.div`
-  background-color: white;
-  height: 300px;
-  border: black 1px solid;
-  width: 200px;
-  border-radius: 10px;
-  z-index: 2;
-
-  font-size: 40px;
+  width: 100px;
+  height: 100px;
+  display: flex;
   justify-content: center;
   align-items: center;
-  display: flex;
-
-  transition: 0.5s ease-in-out;
-  &#pre,
-  &#next {
-    z-index: 1;
-    background-color: #b3b3b3;
-  }
-
-  &#pre {
-    margin-right: -90px;
-  }
-
-  &#next {
-    margin-left: -90px;
-  }
+  border: 1px solid grey;
+  position: absolute;
+  margin-top: 100px;
+  transition: 0.5s;
 `;
