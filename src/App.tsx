b31d9fdb33eeb459f69474a23799a4c1ea0b50cf
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-04-06 22:20:16 +0900

    카드 앞으로 가기 애니메이션

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ let list = [1, 2, 3, 4, 5];
 
 export default function App() {
   const [tempList, setTempList] = useState([4, 5, 1, 2, 3]);
-  // [3,4,5,1,2]
+
   const before = () => {
     const temp = tempList;
     const end = tempList[tempList.length - 1];
@@ -14,10 +14,17 @@ export default function App() {
     setTempList([...temp]);
   };
 
+  const after = () => {
+    const temp = tempList;
+    const start = temp.shift() ?? -1;
+    temp.splice(temp.length, 0, start);
+    setTempList([...temp]);
+  };
+
   return (
     <Wrap>
       <button onClick={before}>뒤로</button>
-      <button>앞으로</button>
+      <button onClick={after}>앞으로</button>
       {list.map((_, index) => (
         <Card
           key={index}
